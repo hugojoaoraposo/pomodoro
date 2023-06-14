33495e276d8d7bcf8c5d4c9a8912ff6f93e34b0f
Hugo <hugojoaoraposo@gmail.com>
2023-06-15 00:35:42 +0100

    teste julio2

--- frontend/src/components/pomodoro/pomodoro.js
+++ frontend/src/components/pomodoro/pomodoro.js
@@ -1,19 +1,20 @@
 import logo from "../../../src/img/logo.png";
-import CountdownCircle from "../countdownCircle/CountdownCircle";
 import NavBar from "../navbar/Navbar";
 import Timer from "../timer/Timer";
 
 export default function Pomodoro() {
     //ler a rota atual
   return (
-    <div className="bg-[#f4f4f9] h-[100vh]">
+    <div>
       <div className="flex flex-col pt-14 items-center pb-8">
         <img src={logo} width={80} alt="Logo" />
       </div>
       <div >
         <Timer/>
-       
         <NavBar />
+        {/* <NavBar selected={
+            //rota atual
+        }/> */}
       </div>
     </div>
   );
--- frontend/src/components/profile/profile.js
+++ frontend/src/components/profile/profile.js
@@ -1,32 +1,20 @@
-import pic from "../../img/gustavosantos.png";
-import DayIcon from "../../icons/today_icon.png";
-import WeekIcon from "../../icons/week_icon.png";
-import MonthIcon from "../../icons/month_icon.png";
-import './Profile.css';
+import pic from "../../img/pictemp.png"
+
+
 
 export default function Profile() {
   return (
-    <div className="profile-container">
+    <div >
       <div className="flex flex-col items-center pt-20">
-        <img className="rounded-full" src={pic} width={80} alt="pic" />
-        <h1 className="pt-5">Gustavo Santos</h1>
-        <div className="w-screen h-72 flex items-center">
-          <ul className="space-y-10 list-none no-underline">
-            <li>
-              <img className="today" src={DayIcon} alt="Today" />
-              Today:
-            </li>
-            <li>
-              <img className="week" src={WeekIcon} alt="This Week" />
-              This Week:
-            </li>
-            <li>
-              <img className="month" src={MonthIcon} alt="This Month" />
-              This Month:
-            </li>
-          </ul>
+        <img className="rounded-full " src={pic} width={80} alt="pic" />
+        <h1 className="pt-5">user name</h1>
+        <div className=" w-screen h-72 flex items-center ">
+          <u className="space-y-10 list-none no-underline">
+            <li>Today:</li>
+            <li>This Week:</li>
+            <li>This Month:</li>
+          </u>
         </div>
-        <a href="/pomodoro" className="return-button">Logout</a>
       </div>
     </div>
   );
