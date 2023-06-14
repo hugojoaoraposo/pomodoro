885a38d2ba87f2853d3b4e4a574fec4189393088
Júlio <Julio@Air-de-Filipa.Home>
2023-06-14 19:46:03 +0100

    Updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,17 @@
-
-
-
-
-
 import { BrowserRouter } from 'react-router-dom'
 import Routes from './Routes'
 import './App.css';
 
 function App() {
   return (
-<>
+
     <BrowserRouter>
       <Routes />
 
     </BrowserRouter>
-<Pomodoro/>
-  {/* <Profile /> */}
-  <NavBar/
-</>
+
+
+
 
   );
 }
--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -2,7 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
 import Login from './components/Login/Login';
-import Profile from './components/Profile/Pcd frrofile'
+import Profile from './components/profile/Profile'
+import Pomodoro from './components/pomodoro/Pomodoro'
 
 
 export default () => {
@@ -11,6 +12,8 @@ export default () => {
             <Route exact path='/' element={<Login />}>
             </Route>
             <Route exact path="/profile" element={<Profile />}>
+            </Route>
+            <Route exact path="/pomodoro" element={<Pomodoro />}>
             </Route>
         </Routes>
     )
