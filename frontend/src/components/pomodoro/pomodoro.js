4e2f397de5ed072145170263115a18f9115e4f42
juliogsmacedo <julio.gsmacedocoding@gmail.com>
2023-06-15 10:14:49 +0100

    julio

--- frontend/src/components/pomodoro/pomodoro.js
+++ frontend/src/components/pomodoro/pomodoro.js
@@ -1,26 +1,21 @@
-
-
 import logo from "../../../src/img/logo.png";
-// import CountdownCircle from "../countdownCircle/CountdownCircle";
 import NavBar from "../navbar/Navbar";
 import Timer from "../timer/Timer";
-import "./Pomodoro.css"
+
 export default function Pomodoro() {
     //ler a rota atual
-
-
-    
   return (
-    <div className=" h-screen overflow-y-hidden pomodoro-container">
-      <div className="flex flex-col pt-14 items-center overflow-y-hidden pb-8">
+    <div>
+      <div className="flex flex-col pt-14 items-center pb-8">
         <img src={logo} width={80} alt="Logo" />
       </div>
       <div >
         <Timer/>
-       
-        <NavBar pages={"pomodoro"} />
+        <NavBar />
+        {/* <NavBar selected={
+            //rota atual
+        }/> */}
       </div>
     </div>
   );
 }
-
